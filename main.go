5a634d113949220596cae0b590a92afab316a16c
Pablo Firulo <pfirulo@yahoo.com>
2024-02-19 21:47:40 -0300

    Main modificado

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/pfirulo2022/fiber-crud-api/database"
+)
 
 func main() {
-	fmt.Print("Hola Pablo!")
+	database.ConnectDB()
+	app := fiber.New()
+
+	app.Use(cors.New(cors.Config{
+		AllowOrigins:     "http://localhost:5173",
+		AllowMethods:     "GET,POST,PUT,DELETE",
+		AllowCredentials: true,
+		AllowHeaders:     "Origin, Content-Type, Accept",
+	}))
+
+	log.Fatal(app.Listen(":8000"))
 }
